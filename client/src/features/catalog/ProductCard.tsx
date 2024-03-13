05a420de37d7abb4687a0f7898112df8368e49a7
Sanjit Muthineni <sanjit.muthineni@credera.com>
2024-03-13 09:27:43 -0400

    react frontend card themes

---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,36 @@
+import { Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { Product } from "../../app/models/product";
+
+interface Props {
+    product: Product;
+}
+
+export default function ProductCard({product}: Props) {
+    return (
+<Card>
+    <CardHeader
+        title={product.name}
+        titleTypographyProps={{
+            sx: {fontWeight: 'bold'}
+        }}
+    />
+      <CardMedia
+        sx={{ height: 200 }}
+        image={product.imageUrl}
+        title={product.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          ${product.price.toFixed(2)}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+            {product.brand} / {product.type}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add to cart</Button>
+        <Button size="small">View</Button>
+      </CardActions>
+    </Card>
+    )
+}
